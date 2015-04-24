373ba082232cc6e8b89b834f17287997e67474bc
Adam Yowanda <yowanda6361@gmail.com>
2015-04-24 09:45:31 +0700

    Add LoginPage and Autofac

--- FlagMySpace/FlagMySpace/App.xaml.cs
+++ FlagMySpace/FlagMySpace/App.xaml.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace FlagMySpace
 {
-    public class App : Application
+    public partial class App
     {
         public App()
         {
